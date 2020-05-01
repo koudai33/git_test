5af38f002a7fa27aacc9fd2fb401ba44a04d7165
koudai <yamaokoudai33@gmail.com>
2020-05-01 23:31:25 +0900

    Fixed code

--- script/foo.js
+++ script/foo.js
@@ -1,29 +1,27 @@
 let fruits = {
-    list: {
-        summer: {
-            fruits: [
-                { name: 'watermelon', colors: 'green', teachProgramming() { } },
-                { name: 'pineapple', colors: 'yellow', teachHistroy() { } }
-            ],
-            teach() { }
-        },
-        winter: {
-            fruits: [
-                { name: 'apple', colors: 'red', }
-            ],
-            makeSmile() {}
-        }
+  list: {
+    summer: {
+      fruits: [
+        { name: "watermelon", colors: "green", teachProgramming() {} },
+        { name: "pineapple", colors: "yellow", teachHistroy() {} },
+      ],
+      teach() {},
     },
-    plan() { },
-    uploadVideo() {
-        var agyo = ['あ', 'い', 'う', 'え', 'お','か','き','く','け','こ',]
-//二重ループを使ったあ行とか行の名前リスト
-for (var na = 0; na < agyo.length; na++){
-    for (var i = 0; i < agyo.length; i++){
-        document.write('<p>' + agyo[na] + agyo[i] + '</p>');
+    winter: {
+      fruits: [{ name: "apple", colors: "red" }],
+      makeSmile() {},
+    },
+  },
+  plan() {},
+  uploadVideo() {
+    var agyo = ["あ", "い", "う", "え", "お", "か", "き", "く", "け", "こ"];
+    //二重ループを使ったあ行とか行の名前リスト
+    for (var na = 0; na < agyo.length; na++) {
+      for (var i = 0; i < agyo.length; i++) {
+        document.write("<p>" + agyo[na] + agyo[i] + "</p>");
+      }
     }
-}
-     }
+  },
 };
 
 export default fruits;
