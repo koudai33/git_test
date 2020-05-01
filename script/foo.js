09df6450eb6e184846d61bf4221406c66a20b3b0
koudai <yamaokoudai33@gmail.com>
2020-05-01 23:27:30 +0900

    Added new folder

---
+++ script/foo.js
@@ -0,0 +1,29 @@
+let fruits = {
+    list: {
+        summer: {
+            fruits: [
+                { name: 'watermelon', colors: 'green', teachProgramming() { } },
+                { name: 'pineapple', colors: 'yellow', teachHistroy() { } }
+            ],
+            teach() { }
+        },
+        winter: {
+            fruits: [
+                { name: 'apple', colors: 'red', }
+            ],
+            makeSmile() {}
+        }
+    },
+    plan() { },
+    uploadVideo() {
+        var agyo = ['あ', 'い', 'う', 'え', 'お','か','き','く','け','こ',]
+//二重ループを使ったあ行とか行の名前リスト
+for (var na = 0; na < agyo.length; na++){
+    for (var i = 0; i < agyo.length; i++){
+        document.write('<p>' + agyo[na] + agyo[i] + '</p>');
+    }
+}
+     }
+};
+
+export default fruits;
---
+++ script/main.js
@@ -0,0 +1,11 @@
+
+import fruits from "./foo";
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'hello ';
+
+fruits.list
+
+fruits.list.summer.fruits[0].name;
+fruits.plan();
+fruits.list.winter.makeSmile();
+
